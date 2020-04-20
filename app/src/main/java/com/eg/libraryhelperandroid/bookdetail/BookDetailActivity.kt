ffaa27d13499df453433d6369d62427e27dc4b1f
makewheels <33256842+makewheels@users.noreply.github.com>
2020-04-20 10:56:33 +0800

    swipeRefreshLayout

--- app/src/main/java/com/eg/libraryhelperandroid/bookdetail/BookDetailActivity.kt
+++ app/src/main/java/com/eg/libraryhelperandroid/bookdetail/BookDetailActivity.kt
@@ -2,6 +2,7 @@ package com.eg.libraryhelperandroid.bookdetail
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.bumptech.glide.Glide
 import com.eg.libraryhelperandroid.R
 import com.eg.libraryhelperandroid.util.RetrofitUtil
@@ -23,10 +24,26 @@ class BookDetailActivity : AppCompatActivity() {
         val intent = intent
         val id = intent.getStringExtra("id") as String
 
+        //加载书的详情
+        loadBookDetail(id)
+
+        //监听下拉刷新
+        swipeRefreshLayout.setOnRefreshListener(SwipeRefreshLayout.OnRefreshListener {
+            loadBookDetail(id)
+        })
+
+    }
+
+    /**
+     * 加载书的详情
+     */
+    private fun loadBookDetail(id: String) {
         //查询书的详情
         val bookService = RetrofitUtil.getBookService()
         bookService.getBookDetailById(id).enqueue(object : Callback<BookDetailResponse> {
             override fun onFailure(call: Call<BookDetailResponse>, t: Throwable) {
+                //取消下拉刷新转动图标
+                swipeRefreshLayout.isRefreshing = false
             }
 
             override fun onResponse(
@@ -42,8 +59,9 @@ class BookDetailActivity : AppCompatActivity() {
                 tv_publishDate.text = bookDetailResponse.publishDate
                 tv_catalog.text = bookDetailResponse.catalog
                 tv_summary.text = bookDetailResponse.summary
+                //取消下拉刷新转动图标
+                swipeRefreshLayout.isRefreshing = false
             }
-        }
-        )
+        })
     }
 }
