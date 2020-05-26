ef9be60f372425e028f5754ba00c56961ea51c48
makewheels <33256842+makewheels@users.noreply.github.com>
2020-05-26 10:23:21 +0800

    android强大是强大，就是太麻烦了，搞不定啊

--- app/src/main/java/com/eg/libraryhelperandroid/MainActivity.kt
+++ app/src/main/java/com/eg/libraryhelperandroid/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         //检查更新
         checkUpdate()
         setClickListener()
