89c401bacec34427cae1aec366f8e366b957d652
makewheels <makewheels>
2021-05-24 10:13:01 +0800

    上下左右

--- app/src/main/java/com/eg/libraryhelperandroid/util/OkHttpUtil.java
+++ app/src/main/java/com/eg/libraryhelperandroid/util/OkHttpUtil.java
@@ -5,7 +5,7 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 
 public class OkHttpUtil {
-    public static String BASE_URL = "http://192.168.0.101:5001/libraryapp";
+    public static String BASE_URL = "http://10.187.11.233:5001/libraryapp";
 //public static String BASE_URL = "http://baidu.server.qbserver.cn:5001/libraryapp";
 
     public static Call getCall(String url) {
